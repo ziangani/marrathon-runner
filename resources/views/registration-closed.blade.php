1cfe047385e40640d7bb89edfa5ffddd1fd68a18
barthkapepula <barthkapepula@gmail.com>
2025-07-18 17:14:01 +0200

    refactor: Remove volunteer participation note from registration closed page

--- resources/views/registration-closed.blade.php
+++ resources/views/registration-closed.blade.php
@@ -48,12 +48,7 @@
                             </svg>
                             <span>Follow us on social media for updates about future events</span>
                         </li>
-                        <li class="flex items-start">
-                            <svg class="flex-shrink-0 h-5 w-5 text-white mr-3 mt-0.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                            </svg>
-                            <span>You can still participate as a volunteer or spectator</span>
-                        </li>
+                        
                     </ul>
                 </div>
 
